6aea0befbf682b1ab205cf68539c05ee2d8b4f85
10craftedmen <emmanrocacastro@gmail.com>
2025-03-22 01:07:45 -0400

    goonmaxing 24/7

---
+++ Assets/BlocksTempFix.cs
@@ -0,0 +1,10 @@
+using UnityEditor;
+
+public class BlocksTempFix
+{/*
+    [MenuItem("Tools/Fix EditorPrefs")]
+    public static void FixPrefs()
+    {
+        EditorPrefs.DeleteKey(null);
+    }*/
+}
---
+++ Assets/buildthing.cs
@@ -0,0 +1,10 @@
+using UnityEditor;
+
+public class buildthing
+{/*
+    [MenuItem("Tools/Fix build")]
+    public static void FixPrefs()
+    {
+        EditorPrefs.DeleteAll(); // Deletes ALL saved preferences
+    }*/
+}
